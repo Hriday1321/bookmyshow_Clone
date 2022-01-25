972e0c8b31a988be0e300771891e9cdea95e3f77
EC2 Default User <hridaygupta100@hotmail.com>
2022-01-25 12:50:00 +0000

    moar compss

--- src/components/Body.elements.js
+++ src/components/Body.elements.js
@@ -7,11 +7,12 @@ export const Img = styled.img`
 
 export const AlignedDiv = styled.div`
     width: 80%;
-    margin: 2% 10%;
+    margin: 0% 10%;
 `;
 
 export const Text = styled.div`
     width: 100%;
+    margin: 2.4% 0 0 0;
     height: 28px;
     -webkit-text-size-adjust: 100%;
     display: flex;
@@ -19,4 +20,17 @@ export const Text = styled.div`
     font-family: Roboto,sans-serif;
     font-size: 1.5em;
     font-weight: 600;
+`;
+
+export const Item = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  background: none;
+  border-radius: 5px;
+  color: #fff;
+  margin: 15px;
+  font-size: 4em;
 `;
