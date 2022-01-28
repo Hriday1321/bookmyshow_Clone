306dd52d0b66d3ab112aa35d912b36e49390db64
EC2 Default User <hridaygupta100@hotmail.com>
2022-01-28 07:12:27 +0000

    started restructuring

---
+++ src/components/Nav/Nav.elements.js
@@ -0,0 +1,65 @@
+import styled from "styled-components/macro";
+
+export const NavDiv = styled.div`
+  background: rgb(51, 53, 69);
+  color: white;
+  height: 65px;
+  display: flex;
+  padding-left: 10%;
+  align-items: center;
+`;
+
+export const NavSearchDiv = styled.div`
+  background: white;
+  display: flex;
+  border-radius: 5px;
+  border: none;
+  height: 50%;
+  width: 40%;
+  margin-left: 1%;
+  border: none;
+  align-content: center;
+`;
+
+export const NavSubDiv = styled.div`
+  background: rgb(34, 37, 57);
+  color: white;
+  height: 40px;
+  display: flex;
+  padding-left: 10%;
+`;
+
+export const NavA = styled.a`
+  text-decoration: none;
+  color: rgb(204, 204, 195);
+  font-weight: 500;
+`;
+
+export const NavInp = styled.input`
+  border: none;
+  height: 90%;
+  width: 90%;
+  outline: none;
+  margin-right: 5px;
+`;
+
+export const NavBut = styled.button`
+  background: rgb(248, 68, 100);
+  color: white;
+  font-weight: 500;
+  padding: 0.5% 1%;
+  border: none;
+  border-radius: 5px;
+  margin-left: 30%;
+`;
+
+export const NavSubBut = styled.button`
+  background: none;
+  color: rgb(204, 204, 195);
+  font-weight: 500;
+  font-size: 0.9em;
+  margin-right: 0.8%;
+  border: none;
+`;
+
+
