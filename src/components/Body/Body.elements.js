866de7b0d4816782a4ebee2f9172a145aa13cf45
EC2 Default User <hridaygupta100@hotmail.com>
2022-02-02 07:27:57 +0000

    moar componentss

--- src/components/Body/Body.elements.js
+++ src/components/Body/Body.elements.js
@@ -3,11 +3,20 @@ import styled from "styled-components/macro";
 export const Img = styled.img`
     width: 80%;
     margin: 3% 10%;
+    border-radius: 0px;
 `;
 
 export const AlignedDiv = styled.div`
     width: 80%;
     margin: 0% 10%;
+`;
+
+export const AltDiv = styled.div`
+  background: rgb(44, 49, 72);
+  color: white;
+  margin-top: 2%;
+  padding: 2% 10% 0 10%;
+  align-items: center;
 `;
 
 export const Text = styled.div`
@@ -22,3 +31,14 @@ export const Text = styled.div`
     font-weight: 600;
 `;
 
+export const AltText = styled.div`
+    width: 100%;
+    margin: 2.4% 0 0 0;
+    height: 28px;
+    display: flex;
+    justify-content: space-between;
+    color: white;
+    font-family: Roboto,sans-serif;
+    font-size: 1.5em;
+    font-weight: 600;
+`;
