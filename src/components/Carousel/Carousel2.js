2c90927e88b14584fd36c0d13460d9389b95a7bf
EC2 Default User <hridaygupta100@hotmail.com>
2022-02-05 14:27:31 +0000

    p close to doneee

--- src/components/Carousel/Carousel2.js
+++ src/components/Carousel/Carousel2.js
@@ -9,52 +9,52 @@ function MyCarousel() {
         <>
             <Carousel breakPoints={breakPoints}>
               <Item> 
-                <a href="https://in.bookmyshow.com/explore/workshops-vita?referrerBase=">
+                <a href="https://in.bookmyshow.com/vita/movies/dune/ET00122526">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC/et00122526-nvybkaqagm-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/explore/health-and-fitness-vita?referrerBase=">
+                <a href="https://in.bookmyshow.com/vita/movies/venom-let-there-be-carnage/ET00122532">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00133391-tkgsejqduu-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/explore/kids-vita?referrerBase=">
+                <a href="https://in.bookmyshow.com/vita/movies/irresistible/ET00301307">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC/et00122532-banwzudamg-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/explore/comedy-shows-vita?referrerBase=">
+                <a href="https://in.bookmyshow.com/vita/movies/fast-and-furious-9/ET00056556">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00301307-pkynbthhfz-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/explore/music-shows-vita?referrerBase=">
+                <a href="https://in.bookmyshow.com/vita/movies/the-lost-leonardo/ET00314733">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00316211-vbezdftums-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/explore/e-sports-vita?referrerBase=">
+                <a href="https://in.bookmyshow.com/vita/movies/the-matrix-resurrections/ET00133391">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00319461-cllpgvksxu-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/explore/upskill-vita?referrerBase=">
+                <a href="https://in.bookmyshow.com/vita/movies/dangerous-2022/ET00319461">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00056556-mppntlfyfm-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/explore/food-and-drinks-workshops-vita?referrerBase=">
+                <a href="https://in.bookmyshow.com/vita/movies/shane/ET00318498">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00318498-yqznmzrbps-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/explore/gaming-vita?referrerBase=">
+                <a href="https://in.bookmyshow.com/vita/movies/freaky/ET00301310">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00314735-aaecvxhtmd-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/explore/arts-and-crafts-workshops-vita?referrerBase=">
+                <a href="https://in.bookmyshow.com/vita/movies/separation/ET00321106">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00112730-mzhewtvhjz-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
