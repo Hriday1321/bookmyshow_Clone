3d074541161f43fd0fd08d485854c1d1b7ef7f4d
EC2 Default User <hridaygupta100@hotmail.com>
2022-02-10 10:16:19 +0000

    fixed links

--- src/components/Carousel/Carousel2.js
+++ src/components/Carousel/Carousel2.js
@@ -14,32 +14,32 @@ function MyCarousel() {
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/venom-let-there-be-carnage/ET00122532">
+                <a href="https://in.bookmyshow.com/vita/movies/the-matrix-resurrections/ET00133391">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00133391-tkgsejqduu-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/irresistible/ET00301307">
+                <a href="https://in.bookmyshow.com/vita/movies/venom-let-there-be-carnage/ET00122532">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC/et00122532-banwzudamg-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/fast-and-furious-9/ET00056556">
+                <a href="https://in.bookmyshow.com/vita/movies/irresistible/ET00301307">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00301307-pkynbthhfz-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/the-lost-leonardo/ET00314733">
+                <a href="https://in.bookmyshow.com/surat/movies/the-witnesses/ET00316211">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00316211-vbezdftums-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/the-matrix-resurrections/ET00133391">
+                <a href="https://in.bookmyshow.com/test/movies/dangerous-2022/ET00319461">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00319461-cllpgvksxu-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/dangerous-2022/ET00319461">
+                <a href="https://in.bookmyshow.com/movies/fast-and-furious-9/ET00056556">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00056556-mppntlfyfm-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
@@ -49,12 +49,12 @@ function MyCarousel() {
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/freaky/ET00301310">
+                <a href="https://in.bookmyshow.com/movies/never-back-down-revolt/ET00314735">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00314735-aaecvxhtmd-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
               <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/separation/ET00321106">
+                <a href="https://in.bookmyshow.com/margherita/movies/the-last-full-measure/ET00112730">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00112730-mzhewtvhjz-portrait.jpg" alt="" width="100%" height="100%" /> 
                 </a>
               </Item>
