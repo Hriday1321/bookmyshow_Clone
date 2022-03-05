57936a4ac14caff5c09d197fed797824cf3c5766
EC2 Default User <hridaygupta100@hotmail.com>
2022-03-05 10:30:23 +0000

    adding sample data into app

--- src/components/Carousel/Carousel2.js
+++ src/components/Carousel/Carousel2.js
@@ -4,15 +4,17 @@ import "./style/carousel.css";
 
 const breakPoints = [{ width: 500, itemsToShow: 3, itemsToScroll: 3, pagination: false, itemPadding: [0,0,0,0]},{ width: 1200, itemsToShow: 5, itemsToScroll: 5, pagination: false, itemPadding: [0,0,0,0]}];
 
-function MyCarousel() {
+function MyCarousel(props) {
     return(
         <>
             <Carousel breakPoints={breakPoints}>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/dune/ET00122526">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC/et00122526-nvybkaqagm-portrait.jpg" alt="Dune" width="100%" height="100%" /> 
+              {props.movies.map((movie, index) => (
+              <Item>
+                <a href="https://in.bookmyshow.com/vita/movies/the-matrix-resurrections/ET00133391">
+                  <img class="poster" src={movie.Poster} alt="movie" width="100%" height="100%" />
                 </a>
               </Item>
+              ))}
               <Item> 
                 <a href="https://in.bookmyshow.com/vita/movies/the-matrix-resurrections/ET00133391">
                     <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00133391-tkgsejqduu-portrait.jpg" alt="Matrix Resurrections" width="100%" height="100%" /> 
