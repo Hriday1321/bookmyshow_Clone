d04f5bc1701ef76195d0603c854f4171496d88c6
EC2 Default User <hridaygupta100@hotmail.com>
2022-03-21 16:02:12 +0000

    just sweeping up changes i no longer plan to make

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
-import React, { useState, useEffect } from 'react';
 import Nav from './components/Nav/Nav';
 import Body from './components/Body/Body';
 import Footer from './components/Footer/Footer';
-import fetch from 'node-fetch';
 
 function App() { 
-  
-	const [movies, setMovies] = useState([]); 
-	
-	const getMovieRequest = async () => {
-		const url = `http://www.omdbapi.com/?apikey=298f6e5e&s=batman`;
-
-		const response = await fetch(url);
-		const responseJson = await response.json();
-
-		if (responseJson.Search) {
-			setMovies(responseJson.Search);
-		}
-	};
-	
-	useEffect(() => {
-		getMovieRequest();
-	}, []);
   
   return (
     <div className="App">
       <Nav />
-      <Body movies={movies}/>
-      <Body movies={movies}/>
+      <Body/>
+      <Body/>
       <Footer />
     </div>
   );
