9a88c6496ab3ae6ea3b2fcd246ab171acaf086c6
EC2 Default User <hridaygupta100@hotmail.com>
2022-04-14 07:23:05 +0000

    incorporating API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React from "react";
+import axios from "axios";
 import Nav from './components/Nav/Nav';
 import Body from './components/Body/Body';
 import Footer from './components/Footer/Footer';
 
+const baseURL = "https://www.omdbapi.com/?apikey=298f6e5e&s=batman";
+
 function App() { 
+  const [post, setPost] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data.Search);
+    });
+  }, []);
   
   return (
     <div className="App">
       <Nav />
-      <Body/>
-      <Body/>
+      <Body movies={post}/>
       <Footer />
     </div>
   );
--- src/components/Carousel/Carousel2.js
+++ src/components/Carousel/Carousel2.js
@@ -2,72 +2,19 @@ import Carousel from "react-elastic-carousel";
 import { Item } from "./Carousel.elements";
 import "./style/carousel.css";
 
-const breakPoints = [{ width: 500, itemsToShow: 3, itemsToScroll: 3, pagination: false, itemPadding: [0,0,0,0]},{ width: 1200, itemsToShow: 5, itemsToScroll: 5, pagination: false, itemPadding: [0,0,0,0]}];
+const breakPoints = [{ width: 500, itemsToShow: 3, itemsToScroll: 3, pagination: false, itemPadding: [0,0,0,0]},{ width: 1200, itemsToShow: 5, itemsToScroll: 3, pagination: false, itemPadding: [0,0,0,0]}];
 
 function MyCarousel(props) {
     return(
         <>
             <Carousel breakPoints={breakPoints}>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/irresistible/ET00301307">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00301307-pkynbthhfz-portrait.jpg" alt="Irresistible" width="100%" height="100%" /> 
-                </a>
-              </Item>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/shane/ET00318498">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00318498-yqznmzrbps-portrait.jpg" alt="Shane" width="100%" height="100%" /> 
-                </a>
-              </Item>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/irresistible/ET00301307">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00301307-pkynbthhfz-portrait.jpg" alt="Irresistible" width="100%" height="100%" /> 
-                </a>
-              </Item>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/shane/ET00318498">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00318498-yqznmzrbps-portrait.jpg" alt="Shane" width="100%" height="100%" /> 
-                </a>
-              </Item>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/irresistible/ET00301307">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00301307-pkynbthhfz-portrait.jpg" alt="Irresistible" width="100%" height="100%" /> 
-                </a>
-              </Item>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/shane/ET00318498">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00318498-yqznmzrbps-portrait.jpg" alt="Shane" width="100%" height="100%" /> 
-                </a>
-              </Item>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/irresistible/ET00301307">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00301307-pkynbthhfz-portrait.jpg" alt="Irresistible" width="100%" height="100%" /> 
-                </a>
-              </Item>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/shane/ET00318498">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00318498-yqznmzrbps-portrait.jpg" alt="Shane" width="100%" height="100%" /> 
-                </a>
-              </Item>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/irresistible/ET00301307">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00301307-pkynbthhfz-portrait.jpg" alt="Irresistible" width="100%" height="100%" /> 
-                </a>
-              </Item>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/shane/ET00318498">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00318498-yqznmzrbps-portrait.jpg" alt="Shane" width="100%" height="100%" /> 
-                </a>
-              </Item>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/irresistible/ET00301307">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00301307-pkynbthhfz-portrait.jpg" alt="Irresistible" width="100%" height="100%" /> 
-                </a>
-              </Item>
-              <Item> 
-                <a href="https://in.bookmyshow.com/vita/movies/shane/ET00318498">
-                    <img src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00318498-yqznmzrbps-portrait.jpg" alt="Shane" width="100%" height="100%" /> 
-                </a>
-              </Item>
+              {props.movies.map((movie, index) => (
+        				<Item> 
+                  <a href="https://in.bookmyshow.com/vita/movies/irresistible/ET00301307">
+                    <img src={movie.Poster} alt={movie.Title} width="100%" height="100%" /> 
+                  </a>
+                </Item>
+        			))}
             </Carousel>
         </>
     );
