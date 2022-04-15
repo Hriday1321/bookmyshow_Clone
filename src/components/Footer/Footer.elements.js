d602a71012c56c0c6b6941f9d87d7049708e11ae
EC2 Default User <hridaygupta100@hotmail.com>
2022-04-15 09:34:35 +0000

    fixing footer

--- src/components/Footer/Footer.elements.js
+++ src/components/Footer/Footer.elements.js
@@ -3,4 +3,11 @@ import styled from "styled-components/macro";
 export const Div = styled.div`
   background: #333;
   color: #ddd;
+  margin: 0 auto;
+`;
+
+export const Svg = styled.svg`
+  display: block;
+  margin: 0 auto;
+  padding: 0.5% 0;
 `;
